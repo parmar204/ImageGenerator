eeef9ceb06b57d39af321ecb1bd70e2c25aff87a
parmar204 <govindparmar57535@gmail.com>
2025-06-26 16:39:27 +0530

    Steps section completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
 
 const Home = () => {
   return (
     <main>
         <Header />
+        <Steps />
     </main>
   )
 }
